797c674aaf60a3b60aa5fd95020c1ac873a974a9
NoxFly <dorian.thivolle@gmail.com>
2019-11-04 00:17:48 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,41 @@
+# Scrolling library
+
+### current CDN link:
+```js
+<script src='https://cdn.jsdelivr.net/gh/NoxFly/scrolling/scroll.min.js'></script>
+```
+
+**Works without Jquery**
+**Works with arrows**
+**does not work on phone**
+
+Think to enable this option only if user is in computer.
+
+Does not work with Safari yet (window.scrollTo).
+
+other cool library: [SVG library](http://github.com/NoxFly/SVG)
+other cool library: [Drag & Drop library](http://github.com/NoxFly/Drag-and-Drop)
+
+## Usage
+
+```js
+var scroll = new Scroll(window, document, elements, transition);
+// elements can be .class, #id or tag name, but prefer .class
+// transition can be either 'smooth' or 'auto' (auto = no transition duration)
+// window and document are default DOM variables
+scroll.disableScroll(); // do this like that user cannot scroll normally between each page
+// it's not necessary, but it can fix some errors
+```
+
+### Something good to know
+
+if user want to reload page while he scrolled, then next scroll (after reloading) will scroll as he's at the top of the document.
+
+To have a better renderer, you can insert following code:
+
+```js
+var scroll = new Scroll(window, document, '.page', 'smooth');
+scroll.disableScroll();
+
+scroll.goTo(0); // this will scroll to the top of the document smoothly while he's reloading !
+```
---
+++ scroll.js
@@ -0,0 +1,105 @@
+/**
+ * COPYRIGHT NOXFLY 2019
+ * Scroll LIB
+ * 
+ * link: https://cdn.jsdelivr.net/gh/NoxFly/scroll/scroll.js
+ * 
+ */
+
+class Scroll {
+    constructor(window, document, elements, transition='auto') {
+        if(transition != "smooth" && transition != "auto") {
+            transition = "auto";
+        }
+
+        this.window = window;
+        this.document = document;
+
+        this.H = this.window.innerHeight;
+        this.elts = elements;
+        this.container = "#wrapper-"+this.elts;
+
+        this.page = 1;
+        this.maxPage = 0;
+
+        this.scroll = 0;
+        this.transition = transition;
+
+        this.document.querySelectorAll(`.${this.elts}`).forEach(p => {this.maxPage++;});
+
+        this.keys = {};
+
+        this.window.addEventListener('wheel', e => {
+            this.scrollPage(e.deltaY > 0);
+        });
+    }
+
+    scrollPage(dir) {
+        if(dir && this.page < this.maxPage) {
+            this.page++;
+        } else if(!dir && this.page > 1) {
+            this.page--;
+        }
+
+
+        let pg = this.document.querySelector(`.${this.elts}-${this.page}`);
+        let pg_scroll = pg.offsetTop;
+
+        this.document.querySelector(this.container).scroll({
+            top: pg_scroll,
+            left: 0,
+            behavior: this.transition
+        });
+    }
+
+    goTo(i) {
+        if(!isNaN(i)) {
+            this.document.querySelector(this.container).scroll({
+                top: i,
+                left: 0,
+                behavior: this.transition
+            });
+        }
+    }
+
+    preventDefault(e) {
+        e = e || window.event;
+        if (e.preventDefault)
+            e.preventDefault();
+        e.returnValue = false;
+    }
+
+    preventDefaultForScrollKeys(e) {
+        if (this.keys[e.keyCode]) {
+            this.preventDefault(e);
+            return false;
+        }
+    }
+    
+    disableScroll() {
+        this.window.onkeydown = e => {
+            if(e.keyCode == 38 || e.keyCode == 40) {
+                e.preventDefault();
+                this.scrollPage(e.keyCode==40);
+            }
+        }
+
+        if (this.window.addEventListener) {
+            this.window.addEventListener('DOMMouseScroll', this.preventDefault, false);
+        }
+
+        this.document.addEventListener('wheel', this.preventDefault, {passive: false});
+        this.window.onwheel = this.preventDefault;
+        this.window.onmousewheel = this.document.onmousewheel = this.preventDefault;
+    }
+    
+    enableScroll() {
+        if (this.window.removeEventListener) {
+            this.window.removeEventListener('DOMMouseScroll', this.preventDefault, false);
+        }
+
+        this.document.removeEventListener('wheel', this.preventDefault, {passive: false});
+        this.window.onmousewheel = this.document.onmousewheel = null;
+        this.window.onwheel = null;
+    }
+}
